35563157d50005f5a783cd25d8c97f62e0b43433
Tony Rim <duc_thinh_03@yahoo.com>
2019-11-08 22:14:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,15 @@
 def roll_call_dwarves(array)
 
-array.each_with_index do |name, index|
+  array.each_with_index do |name, index|
   puts "#{index +1}. #{name}"
 end 
 end 
+
+def summon_captain_planet(array)
+  
+  array.collect do |call|
+    
+
 
 
 
