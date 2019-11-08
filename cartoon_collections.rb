ef39743c0fde1d525ab9806737fdb2b9be7dd41a
Tony Rim <duc_thinh_03@yahoo.com>
2019-11-08 22:13:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,38 +1,11 @@
+def roll_call_dwarves(array)
 
-
-def roll_call_dwarves(array)   
-  
-  array.each_with_index {|name, index|
+array.each_with_index do |name, index|
   puts "#{index +1}. #{name}"
-  }
 end 
 
 
 
-def summon_captain_planet(array)
-planeteer_calls = []
-
-  array.map do |name|
-  planeteer_calls << "#{name.capitalize}!"
-end 
-planeteer_calls
-end
 
 
-
-
-def long_planeteer_calls(array)
-  
-  array.any? do |call|
-  call.length > 4
-end 
-end
-
-
-
-def find_the_cheese(array)
-  array.find do |cheese|
-  cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
-end 
-end
  cheese_types = ["cheddar", "gouda", "camembert"]
